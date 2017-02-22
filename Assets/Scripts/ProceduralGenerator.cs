0ec777e925ae9f8c035ef7002bdeec2b17b177df
Caillou <p.chapelet@rubika-edu.com>
2017-02-22 09:18:47 +0100

    GitInit

---
+++ Assets/Scripts/ProceduralGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenerator : MonoBehaviour {
+
+
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            Generate();
+        }
+    }
+
+    void Generate()
+    {
+
+    }
+}
