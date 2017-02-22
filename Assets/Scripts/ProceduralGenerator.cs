2558745f730c19fbab6dec2b64c3befc494fcbb5
Caillou <p.chapelet@rubika-edu.com>
2017-02-22 16:29:33 +0100

    Updated

--- Assets/Scripts/ProceduralGenerator.cs
+++ Assets/Scripts/ProceduralGenerator.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProceduralGenerator : MonoBehaviour {
+    public float tileSize;
 
-
-
+    private Terrain terrain;
+    
     void Start()
     {
-
+        terrain = GetComponent<Terrain>();
     }
 
     private void Update()
@@ -21,6 +22,16 @@ public class ProceduralGenerator : MonoBehaviour {
 
     void Generate()
     {
+        float[,] heights = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
 
+        for (int i = 0; i < terrain.terrainData.heightmapWidth; i++)
+        {
+            for (int k = 0; k < terrain.terrainData.heightmapHeight; k++)
+            {
+                heights[i, k] = Mathf.PerlinNoise(((float)i / (float)terrain.terrainData.heightmapWidth) * tileSize, ((float)k / (float)terrain.terrainData.heightmapHeight) * tileSize) / 10.0f;
+            }
+        }
+
+        terrain.terrainData.SetHeights(0, 0, heights);
     }
 }
